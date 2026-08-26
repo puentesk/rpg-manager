dfd7f46d96842b1fb131d45b7b061d6a0d4c1082
Kevin Puentes <puentesvasquezk@gmail.com>
2026-08-25 21:41:04 -0500

    [controlador][semana_1] creacion personaje.py correcion

---
+++ flask_app/personaje.py
@@ -0,0 +1,14 @@
+class Personaje:
+    def __init__(self, nombre: str, clase:str, nivel: int, vida: int):
+        self.nombre = nombre
+        self.clase = clase
+        self.nivel = nivel
+        self.vida = vida
+
+def to_dict(self):
+    return {
+        "nombre": self.nombre,
+        "clase": self.clase,
+        "nivel": self.nivel,
+        "vida": self.vida
+    }
