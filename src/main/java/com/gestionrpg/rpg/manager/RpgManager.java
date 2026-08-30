8476158db3d011cde66042d854a80deff92d183b
Kevin Puentes <puentesvasquezk@gmail.com>
2026-08-30 13:50:45 -0500

    [vista][semana-3] agrega formulaario de creacion y tabla dinamica de personajes en Javalin

--- src/main/java/com/gestionrpg/rpg/manager/RpgManager.java
+++ src/main/java/com/gestionrpg/rpg/manager/RpgManager.java
@@ -14,19 +14,34 @@ public class RpgManager {
 
     public static void main(String[] args) {
         
-        var app = Javalin.create().start(7070);
+        /*var app = Javalin.create().start(7070);
         app.get("/personajes", ctx ->{
+            ctx.json(RepositorioPersonajes.listaPersonajes);
+        });*/
+        var app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+        }).start(7070);
+
+        app.get("/personajes", ctx -> {
             ctx.json(RepositorioPersonajes.listaPersonajes);
         });
         
-        app.post("/personajes", ctx->{
+        /*app.post("/personajes", ctx->{
            Personaje nuevo = ctx.bodyAsClass(Personaje.class);
-        
-           RepositorioPersonajes.listaPersonajes.add(nuevo);
-           
+           RepositorioPersonajes.listaPersonajes.add(nuevo);         
            ctx.status(201).json(nuevo);
+        });*/
+        app.post("/personajes", ctx -> {
+            String nombre = ctx.formParam("nombre");
+            String clase = ctx.formParam("clase");
+            int nivel = Integer.parseInt(ctx.formParam("nivel"));
+            int vida = Integer.parseInt(ctx.formParam("vida"));
+
+            Personaje nuevo = new Personaje(nombre, clase, nivel, vida);
+            RepositorioPersonajes.listaPersonajes.add(nuevo);
+            
+            ctx.redirect("/");
         });
-        
         
     }
     
